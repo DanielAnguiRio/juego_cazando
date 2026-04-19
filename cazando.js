6e8392413af54cdcaf9ca9d5ebc2324cfd082262
DanielAnguiRio <anguisacaandres0106@gmail.com>
2026-04-19 03:25:03 -0500

    se agrego constantes y modifico las posiciones de modificar comida y modificar gato

--- cazando.js
+++ cazando.js
@@ -1,19 +1,38 @@
 let canvas;
 let ctx;
 
+const ALTO_GATO=50;
+const ANCHO_GATO=100;
+const ALTO_COMIDA=50;
+const ANCHO_COMIDA=50;
+
 function graficarGato(){
     ctx.fillStyle = "blue";
-    ctx.fillRect(
-        canvas.width / 2 - 50,
-        canvas.height / 2 - 25,
-        100,
-        50
-    );
+    ctx.fillRect(canvas.width / 2 - 50,canvas.height / 2 - 25,ANCHO_GATO,ALTO_GATO);
 }
 
-function iniciar(){
+function graficarComida(){
+    ctx.fillStyle = "red"; 
+    ctx.fillRect(0,0,ALTO_COMIDA,ANCHO_COMIDA);
+}
+
+function iniciarJuego(){
     canvas = document.getElementById("areaJuego");
     ctx = canvas.getContext("2d");
 
     graficarGato();
+    graficarComida();
+}
+
+function graficarRectangulo(x, y, ancho, alto, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
+}
+
+function graficarGato(){
+    graficarRectangulo(canvas.width / 2 - 50,canvas.height / 2 - 25,ANCHO_GATO,ALTO_GATO,"blue");
+}
+
+function graficarComida(){
+    graficarRectangulo(canvas.width - 50,canvas.height - 50,ALTO_COMIDA,ANCHO_COMIDA,"red");
 }
